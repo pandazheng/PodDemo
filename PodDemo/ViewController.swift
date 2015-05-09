089493fa2d4bf362449ea54823cbd00f24d12a3d
pandazheng <543828315@qq.com>
2015-05-09 13:31:41 +0800

    CocoaPod实例
    利用CocoaPod使用AFNetworking的实例代码

---
+++ PodDemo/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  PodDemo
+//
+//  Created by pandazheng on 15/5/9.
+//  Copyright (c) 2015年 pandazheng. All rights reserved.
+//
+
+/*
+platform :ios,'7.0'
+pod "AFNetworking","~>2.5.3"
+pod "Reachability","~>3.2"
+*/
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        var ret : Bool?
+        ret = ios8()
+        if ret == false
+        {
+            println("ios device version is not ios8")
+        }
+        else
+        {
+            println("ios device version is ios8")
+        }
+        
+        let manager = AFHTTPRequestOperationManager()
+        let url = "http://api.openweathermap.org/data/2.5/weather"
+        let urlstring = NSURL(string: url)
+        manager.GET(url, parameters: nil, success: { (operation:AFHTTPRequestOperation!, responseObject:AnyObject!) -> Void in
+            println("JSON" + responseObject.description!)
+            }) { (operation:AFHTTPRequestOperation!, error:NSError!) -> Void in
+            println("Error:" + error.localizedDescription)
+        }
+    }
+    
+    func ios8() -> Bool
+    {
+        //return UIDevice.currentDevice().systemVersion == "8.0"
+        var deviceVersion: NSString = UIDevice.currentDevice().systemVersion
+        
+        println(deviceVersion.substringWithRange(NSMakeRange(0, 1)))
+        
+        return deviceVersion.substringWithRange(NSMakeRange(0, 1)) == "8"
+        
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,3 @@
+platform :ios,'7.0'
+pod "AFNetworking","~>2.5.3"
+pod "Reachability","~>3.2"
